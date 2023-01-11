cd1b7adc89a0aa10fd0632b8e585880c84ae41b3
Kaili Liu <kaili-liu@ou.edu>
2023-01-11 17:13:48 +0000

    added gitignore

--- middle.sh
+++ middle.sh
@@ -1,11 +1,1 @@
-
-
-# script to list the middle of a file
-# $1 represents the filename
-#
-# usage:
-# bash middle.sh [filename][head number fo lines][tail number of lines] #
-# example
-# bash middle.sh unicorn 50 7
-
-head -n $2 $1 | tail -n $3
+head -n 75 basilisk.dat | tail -n 5
