194ac92c31b59df3504b5887b9af762332ece6cb
Kaili Liu <kaili-liu@ou.edu>
2023-01-11 16:57:01 +0000

    added variables for number of lines

--- middle.sh
+++ middle.sh
@@ -4,7 +4,8 @@
 # $1 represents the filename
 #
 # usage:
-# bash middle.sh [filename] #
+# bash middle.sh [filename][head number fo lines][tail number of lines] #
+# example
+# bash middle.sh unicorn 50 7
 
-
-head -n 75 $1 | tail -n 5
+head -n $2 $1 | tail -n $3
